a8d22c2ba9082d1384d60db0ffab9c20f3fa546f
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2024-03-25 05:33:14 +0200

    adding sxhkd, autostart.sh and picom + editing config.h

--- autostart.sh
+++ autostart.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
-picom &
-volnoti -t 2 &
 nitrogen --restore &
-killall unclutter && unclutter &
-killall udiskie && udiskie &
-killall deadd-notificatoin-center && deadd-notificatoin-center &
-bash ~/Scripts/keyboard_config.sh &
-bash ~/Scripts/screen_layout/two_monitors.sh &
-bash ~/Scripts/volume_control/volume_subscribe.sh &
+sxhkd -c ~/.config/dwm/sxhkd/sxhkdrc &
+unclutter &
+udiskie &
 xset s off -dpms &
 amixer set Capture 40% &
+deadd-notificatoin-center &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 gpaste-client start &
 sudo virsh net-start default &
-xss-lock --transfer-sleep-lock -- i3lock -n -e -i /mnt/Disk_D/Backgrounds/locks/catlock.png &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+sudo chown "$USER":"$USER" /sys/class/backlight/intel_backlight/brightness &
+xss-lock --transfer-sleep-lock -- xfce4-screensaver-command -l &
+xinput set-prop '2.4G Mouse' 'libinput Accel Speed' 0.5 &
 xinput set-prop 'Synaptics TM3336-004' 'libinput Accel Speed' 0.5 &
 xinput set-prop 'Synaptics TM3336-004' 'libinput Tapping Enabled' 1 &
 xinput set-prop 'Synaptics TM3336-004' 'libinput Natural Scrolling Enabled' 1 &
 xinput set-prop 'PixArt Lenovo USB Optical Mouse' 'libinput Accel Speed' 0.3 &
+killall volume_subscribe.sh ; ~/Scripts/volume_control/volume_subscribe.sh &
+bash ~/Scripts/screen_layout/two_monitors.sh &
+bash ~/Scripts/keyboard_config.sh &
+xfce4-screensaver &
+volnoti -t 2 &
+picom --config ~/.config/dwm/picom/picom.conf &
--- config.h
+++ config.h
@@ -9,7 +9,6 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
 static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#1e1e2e";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -62,46 +61,45 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[]    = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *roficmd[]    = { "rofi", "-show", "drun", "-theme", "~/.config/rofi/launcher.rasi", NULL };
 static const char *termcmd[]     = { "kitty", NULL };
-static const char *browsercmd[]  = { "thorium-browser", NULL };
+static const char *browsercmd[]  = { "thorium-browser", "--test-type", NULL };
 
 #include "movestack.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_b,      spawn,          {.v = browsercmd } },
+  { MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+  { MODKEY|ShiftMask,             XK_h,      hide,           {0} },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_j,      focusstackvis,  {.i = +1 } },
+  { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+  { MODKEY|ShiftMask,             XK_j,      focusstackhid,  {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstackvis,  {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
+  { MODKEY|ShiftMask,             XK_k,      focusstackhid,  {.i = -1 } },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+  { MODKEY|ShiftMask,             XK_m,      zoom,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+  { MODKEY|ShiftMask,             XK_r,      quit,           {1} }, 
   { MODKEY,                       XK_s,      show,           {0} },
+  { MODKEY|ShiftMask,             XK_s,      showall,        {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+  { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
+  { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
   { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+  { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = roficmd } },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+  { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
   { MODKEY,                       XK_Tab,    view,           {0} },
   { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_h,      hide,           {0} },
-	{ MODKEY|ShiftMask,             XK_j,      focusstackhid,  {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_k,      focusstackhid,  {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ShiftMask,             XK_s,      showall,        {0} },
-  { MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
-  { MODKEY|ShiftMask,             XK_r,      quit,           {1} }, 
   { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-  { MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
-  { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
